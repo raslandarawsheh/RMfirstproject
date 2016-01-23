b5f5f4092bf9ed02edf71102f0033a006f82d068
Raslan <raslan.darawsheh@gmail.com>
2016-01-23 22:33:58 +0200

    small fixes

---
+++ site1/signup.php
@@ -0,0 +1,29 @@
+<?php
+
+function validate_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if(empty($_POST["email"]))
+		echo "Required Email Address to login"."</br>";
+	else
+		{
+			$email = validate_input($_POST["email"]);
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL))			
+				echo "Wrong Email address\n";
+			else
+				echo "Email: $email"."<br/>";
+		}
+		
+	if(empty($_POST["password"]))
+		echo "Required Password" ."</br>";
+	else 
+		echo "Password: ". validate_input($_POST["password"])."<br/>";
+	
+}
+
+?>
